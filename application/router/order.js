d16f6a5e6540ff108896fdc9772ce3f0d3fa2199
Tung Lam Nguyen Ba <tunglam.ngxba@gmail.com>
2021-07-25 11:12:24 +0700

    a

--- application/router/order.js
+++ application/router/order.js
@@ -3,14 +3,14 @@ var orderService = require("../../domain/services/orderService");
 var auth = require("../../config/auth")
 var router = express.Router();
 
-router.post("/createOrder", auth.require, async (req, res) => {
+router.post("/createOrder", async (req, res) => {
   const orderOps = {};
   for (const [key, value] of Object.entries(req.body)) {
     orderOps[key] = value;
   }
   try {
     const newOrder = await orderService.createOrder(orderOps);
-    res.json(newOrder);
+  res.json(newOrder);
   } catch (err) {
     res.status(400);
     res.json({
@@ -19,7 +19,7 @@ router.post("/createOrder", auth.require, async (req, res) => {
   }
 });
 
-router.get("/:userId", auth.require, async (req, res) => {
+router.get("/:userId",  async (req, res) => {
   const { userId } = req.params;
   try {
     const result = await orderService.getAllOrderOfOwner(userId);
